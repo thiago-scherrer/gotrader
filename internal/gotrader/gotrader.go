e99aaf3bae0a08a92577bdc03a19d926178a7e68
thiago-scherrer <eu@thiago.info>
2019-02-26 00:41:56 -0300

    clean code

--- internal/gotrader/gotrader.go
+++ internal/gotrader/gotrader.go
@@ -13,17 +13,9 @@ func main() {
 func daemonize() {
 	initFlag()
 
-	asset := asset()
-	candleTime := candle()
-	hand := getHand()
-
 	msg1 := "Starting a new round! GoTrader!"
-	msg2 := "Asset: " + asset + "Candle time: " + string(candleTime) + "Hand: " + string(hand)
-
 	fmt.Println(msg1)
 	telegramSend(msg1)
-	fmt.Println(msg2)
-	telegramSend(msg2)
 
 	typeOrder := candleRunner()
 	waitCreateOrder()
