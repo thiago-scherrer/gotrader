b283980354cfd77173e473dbbf990ba73d5eef58
thiago-scherrer <eu@thiago.info>
2019-03-05 20:16:00 -0300

    create tdd to config reader

--- internal/gotrader/bones_test.go
+++ internal/gotrader/bones_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -69,5 +70,293 @@ func TestLastPriceJson(t *testing.T) {
 
 	if getResult != 10.1 {
 		t.Error("LastPrice json parser not working, got:", getResult)
+	}
+}
+
+func Test_initFlag(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "../../configs/config-test.yml"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := initFlag(); got != tt.want {
+				t.Errorf("initFlag() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_configReader(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Conf
+	}{
+		{"Test", configReader()},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := configReader(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("configReader() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_asset(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "XBTUSD"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := asset(); got != tt.want {
+				t.Errorf("asset() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_candle(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"Test", 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := candle(); got != tt.want {
+				t.Errorf("candle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_depth(t *testing.T) {
+	tests := []struct {
+		name string
+		want int64
+	}{
+		{"Test", 30},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := depth(); got != tt.want {
+				t.Errorf("depth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_endpoint(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "https://testnet.bitmex.com"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := endpoint(); got != tt.want {
+				t.Errorf("endpoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_hand(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"Test", 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hand(); got != tt.want {
+				t.Errorf("hand() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_leverage(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "0.1"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := leverage(); got != tt.want {
+				t.Errorf("leverage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_profit(t *testing.T) {
+	tests := []struct {
+		name string
+		want float64
+	}{
+		{"Test", 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := profit(); got != tt.want {
+				t.Errorf("profit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_secret(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "chNOOS4KvNXR_Xq4k4c9qsfoKWvnDecLATCRlcBwyKDYnWgO"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := secret(); got != tt.want {
+				t.Errorf("secret() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_threshold(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"Test", 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threshold(); got != tt.want {
+				t.Errorf("threshold() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_userid(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "LAqUlngMIQkIUjXMUreyu3qn"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := userid(); got != tt.want {
+				t.Errorf("userid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_telegramUse(t *testing.T) {
+	tests := []struct {
+		name string
+		want bool
+	}{
+		{"Test", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := telegramUse(); got != tt.want {
+				t.Errorf("telegramUse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_telegramKey(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "xxxxx"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := telegramKey(); got != tt.want {
+				t.Errorf("telegramKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_telegramurl(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "https://api.telegram.org"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := telegramurl(); got != tt.want {
+				t.Errorf("telegramurl() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_telegramChannel(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"Test", "@"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := telegramChannel(); got != tt.want {
+				t.Errorf("telegramChannel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_verboseMode(t *testing.T) {
+	tests := []struct {
+		name string
+		want bool
+	}{
+		{"Test", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := verboseMode(); got != tt.want {
+				t.Errorf("verboseMode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_speed(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"Test", 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := speed(); got != tt.want {
+				t.Errorf("speed() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
