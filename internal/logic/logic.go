0b0b7be270a36bb2ba891f0707e7ea37ca81c4d5
r0b0tnull <eu@thiago.info>
2019-07-08 20:31:33 -0300

    add logic tamplate

---
+++ internal/logic/logic.go
@@ -0,0 +1,22 @@
+package logic
+
+import (
+	"fmt"
+)
+
+// CandleRunner verify the api and start the logic system
+func CandleRunner() string {
+	return "Example of candle"
+}
+
+// ClosePositionProfitBuy the Buy position
+func ClosePositionProfitBuy() bool {
+	fmt.Println("Example of logic")
+	return false
+}
+
+// ClosePositionProfitSell close the Sell position
+func ClosePositionProfitSell() bool {
+	fmt.Println("Example of logic")
+	return false
+}
